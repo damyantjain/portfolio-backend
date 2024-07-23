fb971a5ddf0475f9cab8dbf74702211543f479f4
damyantjain <damyant9999@gmail.com>
2024-07-23 19:05:21 -0400

    Test change for github workflow

--- App.js
+++ App.js
@@ -44,5 +44,5 @@ BlogRoutes(app);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
